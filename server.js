07305ccdc21fd4a06a24806e76c97f43412346fd
meridianon <pihab.bee@gmail.com>
2017-10-25 01:18:30 +0300

    first react-app

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(__dirname));
+
+app.listen(app.get('port'), function () {
+	console.log('Server start http:localhost' + app.get('port') + '/');
+});
